627291b08d071df71de6e665cc21d78f4b11dd66
RehamMobarak <reham.a.mobarak@gmail.com>
2021-06-13 04:15:41 +0200

    basic blades

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Homepage</h1>
+@endsection
+
+@section('sidebar')
+    @parent
+    this is appended
+@endsection
+
---
+++ resources/views/inc/sidebar.blade.php
@@ -0,0 +1,10 @@
+@section('sidebar')
+<div class="sidebar">
+    <h2>Sidebar</h2>
+<ul>
+    <li>item 1</li>
+    <li>item 2</li>
+    <li>item 3</li>
+</ul>
+@show
+</div>
