fd65b0a0107f8ee98d7a4aaaae034a18b97a393b
RehamMobarak <reham.a.mobarak@gmail.com>
2021-06-14 00:05:53 +0200

    add navbar,footer basic styling

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -1,10 +1,10 @@
 @section('sidebar')
-<div class="sidebar">
+<div class="card">
     <h2>Sidebar</h2>
-<ul>
-    <li>item 1</li>
-    <li>item 2</li>
-    <li>item 3</li>
-</ul>
-@show
+    <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+    </ul>
 </div>
+@show
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,28 @@
     <title>Basic Website</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
+
 <body>
-    @yield('content')
-    @include('inc.sidebar')
+    @include('inc.navbar')
+
+    <div class="container">
+        @if (Request::is('/'))
+        @include('inc.showcase')
+        @endif
+
+        <div class="row">
+            <div class="col-md-8 col-lg-8">
+                @yield('content')
+            </div>
+            <div class="col-md-4 col-lg-4">
+                @include('inc.sidebar')
+            </div>
+        </div>
+    </div>
+    <footer id='footer' class="text-center">
+        <p>Copyright 2021 &copy; Reham</p>
+    </footer>
+
 </body>
+
 </html>
