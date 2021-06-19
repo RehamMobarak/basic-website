6dc8527566f08297c7bf4ee81a22561380c1b661
RehamMobarak <reham.a.mobarak@gmail.com>
2021-06-19 02:39:26 +0200

    ContactContsubmit save message

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactRequest;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
@@ -10,6 +11,14 @@ class ContactController extends Controller
     public function submit(ContactRequest $request)
     {
         // dd($request->input('name'));
-        return 123;
+        // return 123;
+        $message = new Message();
+        // -> DB fields name == -> input names
+        $message->Name = $request->input('name');
+        $message->Subject = $request->input('subject');
+        $message->Email = $request->input('email');
+        $message->Message = $request->input('message');
+        $message->save();
+        return redirect()->route('home')->with('success','Message succefuly sent! Thank you');
     }
 }
