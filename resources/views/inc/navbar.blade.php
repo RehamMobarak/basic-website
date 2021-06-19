c91ece402c1c407106e6c4b2283759089a889dcc
RehamMobarak <reham.a.mobarak@gmail.com>
2021-06-19 04:19:20 +0200

    edit: route names instead of links in navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -8,13 +8,16 @@
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
         <li class="nav-item">
-          <a class="nav-link" href="/">Home</a>
+          <a class="nav-link" href="{{route('home')}}">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/about">About</a>
+          <a class="nav-link" href="{{route('about')}}">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/contact">Contact</a>
+          <a class="nav-link" href="{{route('contact')}}">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('get-messages')}}">See contact messages</a>
         </li>
       </ul>
     </div>
